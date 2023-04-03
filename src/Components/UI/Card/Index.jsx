88778ecdf8750d8b588451f9a4d9963b27c2b31a
unknown <proeslros2@gmail.com>
2023-04-03 12:43:04 +0200

    :sparkles: Added a new feature: switch dark mode and light mode, all CSS is done

--- src/Components/UI/Card/Index.jsx
+++ src/Components/UI/Card/Index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
 function Card(props) {
+
+    console.log('props:', props);
+
     return (
-        <article className='card'>
+        <article className={props.theme === 'light' ? 'card bcg-light' : 'card'}>
             <div className='card-header'>
-                <div className='card-header-icons'>
+                <div className={props.theme === 'light' ? 'card-header-icons bcg2-light' : 'card-header-icons'}>
                     <svg viewBox="0 0 1024 1024" display="inline-block" stroke="currentcolor" fill="currentcolor" width="32px" height="32px">
                         <path d={props.path}/>
                     </svg>
                 </div>
-                <h2>{props.title}</h2>
+                <h2 className={props.theme === 'light' ? 'txt-light' : ''}>{props.title}</h2>
             </div>
-            <p>{props.para1}</p>
+            <p className={props.theme === 'light' ? 'txt-light' : ''}>{props.para1}</p>
         </article>
     )
 }
--- src/Components/UI/Project/Index.jsx
+++ src/Components/UI/Project/Index.jsx
@@ -5,8 +5,8 @@ function Project(props) {
         <section className='card-project'>
             <img src={props.img} alt={props.alt} />
             <article className='card-project-about'>
-                <h2>{props.title}</h2>
-                <p>{props.para}</p>
+                <h2 className={props.theme === 'light' ? 'txt-light' : ''}>{props.title}</h2>
+                <p className={props.theme === 'light' ? 'txt-light' : ''}>{props.para}</p>
                 <a href='https://github.com/BlackPearlsDev' target={'_blank'} rel='noreferrer'>Voir notre travail</a>
             </article>
         </section>
